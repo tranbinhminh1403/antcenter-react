08f214b6c71493721821694be83fcc9a0bc7a9c6
tranbinhminh1403 <myemail@example.com>
2024-11-14 14:25:06 +0700

    update: on hover general button color

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://tranbinhminh1403.github.io/deploy-github/",
   css: {
     preprocessorOptions: {
       scss: {
